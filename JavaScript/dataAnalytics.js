1657d2bab1ae74694208ad7bd1df53117dd4843c
ianfanArthrex <55906354+QihangFan@users.noreply.github.com>
2024-05-19 21:49:55 -0400

    Revert to the previous property

--- JavaScript/dataAnalytics.js
+++ JavaScript/dataAnalytics.js
@@ -2,7 +2,7 @@
     // Create a script element
     var script = document.createElement('script');
     script.async = true;
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-L01WP1SELJ'; // Replace with your GA4 Measurement ID
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-3BFP82C12P'; // Replace with your GA4 Measurement ID
   
     // Insert the script element into the head
     document.head.appendChild(script);
@@ -13,7 +13,7 @@
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
       
-        gtag('config', 'G-L01WP1SELJ');
+        gtag('config', 'G-3BFP82C12P');
     };
   })();
   
